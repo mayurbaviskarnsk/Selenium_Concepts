bfcc1da8256412017c2e0e3f60b465547851f6f3
mayurbaviskarnsk <mayurbaviskarnsk@gmail.com>
2024-02-21 00:01:03 +0530

    selenium commands added

--- selenium_commands.py
+++ selenium_commands.py
@@ -1,9 +1,14 @@
 # Browser Commands
 # Navigation Commands
 # WebElement Commands
+import time
+
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 url = "https://www.naukri.com/nlogin/login"
 driver = webdriver.Chrome()
 
 driver.get(url)
+time.sleep(20)
+driver.find_element(By.XPATH,'//*[@id="usernameField"]').send_keys("12345")
